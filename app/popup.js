d1360d64deec91982f4de4382b3a2b9b77970d95
Gilles Piou <gilles.piou@gmail.com>
2014-07-25 14:16:58 +0200

    Add popup

--- app/popup.js
+++ app/popup.js
@@ -3,11 +3,29 @@ angular
   .controller('popupCtrl', ['$scope', function($scope) {
 
     chrome.storage.local.get(null, function(value) {
-      console.log(value);
       $scope.$apply(function() {
-        $scope.queries = value.queries || [];
-        if ($scope.queries.length === 0) $scope.queries.push({ });
+        var firstKey = _.keys(value.results)[0]
+        $scope.allResults = value.results || [];
+        $scope.seenResults = value.results[firstKey];
+        $scope.current = firstKey;
       });
     });
 
-}]);
+    $scope.seeResults = function(key) {
+      $scope.current = key;
+      $scope.seenResults = $scope.allResults[key];
+    };
+
+    $scope.seeItem = function(item) {
+      chrome.tabs.create({ url: item.url });
+    };
+
+  }])
+  .filter('beautify', function() {
+
+    return function(input) {
+      var matches = input.match(/q=([^&]*)/);
+      return matches[1];
+    }
+
+  });
